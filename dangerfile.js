ee7d0666f3bebe3225a3ce050bb8013b8e72f45d
C0der Lint <no1can@pm.me>
2019-03-05 19:38:55 +0530

    danger file changes

--- dangerfile.js
+++ dangerfile.js
@@ -1,4 +1,4 @@
-import {message, danger} from "danger"
+import {message, danger, fail} from "danger"
 
 const modified = danger.git.modified_files
 const newFiles = danger.git.created_files
@@ -6,5 +6,18 @@ const prTitle = danger.github.pr.title
 
 console.log(danger.git, danger.github.pr)
 
-message(`Modified files: ${modified}`)
-message(`Title of PR: ${prTitle}`)
+// Check title of PR
+if(prTitle.match(/[\d\w]+?\.js\.org/))
+  message(`Title of PR: ${prTitle}`)
+else
+  fail(`Title of Pull Request is not in the format *myawesomeproject.js.org*`)
+
+// Check number of modified files and if the right file is modified
+if(modified.length == 1 && modified[0] == "cnames_active.js")
+  message(":white_check_mark: Only file modified is cnames_active.js")
+else {
+  warn("Multiple files modified")
+  message(`Modified files: ${modified.join(", ")}`)
+}
+
+// JSON.parse()
