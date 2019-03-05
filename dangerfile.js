a0ba981262a7bc6e5b657d8f6537a49b05b012a4
C0der Lint <no1can@pm.me>
2019-03-05 21:48:07 +0530

    lets do it

--- dangerfile.js
+++ dangerfile.js
@@ -6,29 +6,37 @@ const modified = danger.git.modified_files;
 const newFiles = danger.git.created_files;
 const prTitle = danger.github.pr.title;
 
+const isActiveFileModified = () => {
+  return new Promise((resolve, reject) => {
+    // Check number of modified files and if the right file is modified
+    if(modified.includes(activeFile))
+      if(modified.length == 1) {
+        message(`:white_check_mark: Only file modified is ${activeFile}`)
+        resolve();
+      }
+      else
+        warn(`Multiple files modified - ${modified.join(", ")}`)
+    else 
+      fail(`${activeFile} not modified.`)
+    reject();
+  })
+}
+
+const checkDiff = async () => {
+  let diff = JSON.parse(await danger.git.diffForFile(activeFile));
+  if(diff.added.match(/^\+\s{2},"[\d\w]+?":\s".+?"$/))
+    message(`:white_check_mark: Kudos, you've correctly added you domain addition.`)
+  else
+    message(`Regex failed for \`${diff.added}\``)
+}
 
 // Check title of PR
 if(prTitle.match(/[\d\w]+?\.js\.org/))
   message(`Title of PR: ${prTitle}`)
 else
-  fail(`Title of Pull Request is not in the format *myawesomeproject.js.org*`)
+  warn(`Title of Pull Request is not in the format *myawesomeproject.js.org*`)
 
-// Check number of modified files and if the right file is modified
-if(modified.includes(activeFile))
-  if(modified.length == 1)
-    message(`:white_check_mark: Only file modified is ${activeFile}`)
-  else
-    warn(`Multiple files modified - ${modified.join(", ")}`)
-else 
-  fail(`${activeFile} not modified.`)
-
-danger.git.diffForFile(activeFile).then( data => {
-  console.log(`Diff: ${JSON.stringify(data)}`);
-})
-
-danger.git.structuredDiffForFile(activeFile).then( data => {
-  console.log(`Structured Diff: ${JSON.stringify(data)}`);
-})
-
+isActiveFileModified()
+  .checkDiff();
 
 // JSON.parse()
