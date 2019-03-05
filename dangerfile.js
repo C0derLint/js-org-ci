dd3902f0a6169d9d4815129157cf94108cf786d4
C0der Lint <no1can@pm.me>
2019-03-05 23:19:04 +0530

    k

--- dangerfile.js
+++ dangerfile.js
@@ -6,42 +6,58 @@ const modified = danger.git.modified_files;
 const newFiles = danger.git.created_files;
 const prTitle = danger.github.pr.title;
 
-const isActiveFileModified = () => {
-  return new Promise((resolve, reject) => {
-    // Check number of modified files and if the right file is modified
-    if(modified.includes(activeFile)) {
-      if(modified.length == 1)
-        message(`:white_check_mark: Only file modified is ${activeFile}`)
-      else
-        warn(`Multiple files modified - ${modified.join(", ")}`)
-      resolve();
-    } else {
-      fail(`${activeFile} not modified.`)
-      reject();
-    }
-  })
-}
+// const checkIfActiveFileModified = () => {
+  
+// }
 
-const checkDiff = async () => {
-  let diff = await danger.git.diffForFile(activeFile);
-  if(diff.added.match(/^\+\s{2},"[\d\w]+?":\s".+?"$/))
-    message(`:white_check_mark: Kudos, you've correctly added you domain addition.`)
-  else
-    message(`Regex failed for \`${diff.added}\``)
-}
+// const checkDiff = async () => {
+//   let diff = await danger.git.diffForFile(activeFile);
+//   let linesOfCode = await danger.git.linesOfCode();
 
-//const checkPrTitle = async () => {
-//  resolve(titleMatch[1]);
-//}
+//   if(diff.added.match(/^\+\s*?,"[\d\w]+?":\s*?".+?"$/) && linesOfCode == 1) {
+//     message(`:heavy_check_mark: Only one line added!`)    
+//   } else
+//     warn(`Regex failed for \`${diff.added}\``)
+// }
 
-let titleMatch = /([\d\w]+?)\.js\.org/.exec(prTitle)
-if(titleMatch != null)
-  message(`Title of PR: ${prTitle}`)
-else
-  warn(`Title of Pull Request is not in the format *myawesomeproject.js.org*`)
 
-isActiveFileModified()
-  .then(() => checkDiff())
-  .catch(() => console.log(`${activeFile} isn't even modified!`));
+// isActiveFileModified()
+//   .then(() => checkDiff())
+//   .catch(err => console.log(err));
 
 // JSON.parse()
+
+const result = async () => {
+  
+  // Check if cnames_active.js is modified.
+  let isCNamesFileModified = modified.includes(activeFile);
+
+  if(isCNamesFileModified)
+    if(modified.length == 1)
+      message(`:heavy_check_mark: Only file modified is ${activeFile}`)
+    else
+      warn(`Multiple files modified - ${modified.join(", ")}`)
+  else
+    fail(`${activeFile} not modified.`)
+
+
+  // Check if PR title matches *.js.org
+  let prTitleMatch = /([\d\w]+?)\.js\.org/.exec(prTitle)
+  let isPRTitleCorrect = prTitleMatch !== null
+
+  if(isPRTitleCorrect)
+    message(`:heavy_check_mark: Title of PR - ${prTitle}`)
+  else
+    warn(`Title of Pull Request is not in the format *myawesomeproject.js.org*`)
+
+  
+  // Check number of lines changed in diff
+  let linesOfCode = await danger.git.linesOfCode();
+
+  if(linesOfCode == 1)
+    message(`:heavy_check_mark: Only one line added!`)    
+  else
+    warn(`More than one line added! There's no need to write essays here ;)`)
+}
+
+result().catch(() => console.log("Well .. humbug"));
