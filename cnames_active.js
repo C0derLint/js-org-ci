51a600c5713c7f07d49a06028d07e8968483dda4
Lint <44368997+C0derLint@users.noreply.github.com>
2019-03-06 16:02:38 +0530

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -49,6 +49,7 @@ var cnames_active = {
   ,"23chemistrycamp": "nchuchemistry.github.io/23chemistrycamp"
   ,"2tube": "you2php.github.io/you2php"
   ,"404": "licshee.github.io/404"
+  ,"404": "404page.com"
   ,"76": "hustcc.github.io/76"
   ,"766": "766.github.io"
   ,"7anshuai": "7anshuai.github.io" // noCF? (don't add this in a new PR)
